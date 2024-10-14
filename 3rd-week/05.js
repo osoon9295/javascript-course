1638c9dbe6d28f4dc6eab69038aa2f6da93197e8
Boyoung Lee <genuss040822@gamil.com>
2024-10-14 21:40:59 +0900

    3주차 3-5 강의메모 자바스크립트 파일

---
+++ 3rd-week/05.js
@@ -0,0 +1,10 @@
+var n = null;
+console.log(typeof n); // object : 자바스크립트의 유명한 버그
+
+// 동등연산자(equality operator)
+console.log(n == undefined); // true
+console.log(n == null); // true
+
+// 일치연산자(identity operator)
+console.log(n === undefined); // false
+console.log(n === null); // true
