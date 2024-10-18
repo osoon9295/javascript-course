0b4a442698253872cd48dad9d0fdb87bc3fbb64b
Boyoung Lee <osoon9295@gmail.com>
2024-10-18 20:18:30 +0900

    4주차 4-7 강의메모 자바스크립트 파일

---
+++ 4th-week/07.js
@@ -0,0 +1,76 @@
+// 비동기적 코드의 이해!
+// setTimeout은 대표적인 비동기
+
+// setTimeout(function () {
+//   console.log("여기가 먼저일까?");
+// }, 1000);
+
+// console.log("여기좀 봐주세요!");
+
+//-----------------------------------------------
+
+setTimeout(
+  function (name) {
+    var coffeeList = name;
+    console.log(coffeeList);
+
+    setTimeout(
+      function (name) {
+        coffeeList += ", " + name;
+        console.log(coffeeList);
+
+        setTimeout(
+          function (name) {
+            coffeeList += ", " + name;
+            console.log(coffeeList);
+
+            setTimeout(
+              function (name) {
+                coffeeList += ", " + name;
+                console.log(coffeeList);
+              },
+              500,
+              "카페라떼"
+            );
+          },
+          500,
+          "카페모카"
+        );
+      },
+      500,
+      "아메리카노"
+    );
+  },
+  500,
+  "에스프레소"
+);
+
+//-----------------------------------------------
+
+// 기명함수로 변환
+var coffeeList = "";
+
+var addEspresso = function (name) {
+  coffeeList = name;
+  console.log(coffeeList);
+  setTimeout(addAmericano, 500, "아메리카노");
+};
+
+var addAmericano = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+  setTimeout(addMocha, 500, "카페모카");
+};
+
+var addMocha = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+  setTimeout(addLatte, 500, "카페라떼");
+};
+
+var addLatte = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+};
+
+setTimeout(addEspresso, 500, "에스프레소");
